7387fa1f841110c23506904e3e988fab6711833f
anasbari09-stack <anasbari09@gmail.com>
2026-02-21 10:42:15 +0000

    feat: setup Part 4 skeleton and finished exercise 01

---
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -0,0 +1,11 @@
+
+while True:
+    editor = input("Editor: ")
+    editor = editor.lower()
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    elif editor == "word" or editor == "notepad":
+        print("awful")
+    else:
+        print("not good")
