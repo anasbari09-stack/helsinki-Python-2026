c0bc80b84fa7a6b918d7a39400e0638d9d1cb81d
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 12:43:49 +0000

    solved part 4 exercise 9 The greatest number with logic

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,5 +1,12 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def greatest_number(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > c:
+        return b
+    else:
+        return c
 if __name__ == "__main__":
-    greatest = greatest_number(5, 4, 8)
+    greatest = greatest_number(10, 12, 8)
     print(greatest)
