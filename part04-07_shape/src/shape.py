156a52b3571ab631c83c707917b7964b4d4a92e9
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 10:31:59 +0000

    Solved Part 4 Exercise 7 A shape

--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,24 @@
 # Copy here code of line function from previous exercise and use it in your solution
 # You can test your function by calling it within the following block
+def line(number, string):
+    if string == "":
+        print("*" * number)
+    else:
+        print(string[0] * number)
+if __name__ == "__main__":
+    line(5, "xxx")
+
+def shape(width, char, height, char2):
+    i = 1
+    while i <= width:
+        line(i, char)
+        i += 1
+    while height > 0:
+        line(width, char2)
+        height -= 1
+
+
+    
+
 if __name__ == "__main__":
     shape(5, "x", 2, "o")
