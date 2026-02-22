00c375e7c313753fc7825a9d3eed47d2ce74fb05
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 12:16:51 +0000

    Solved Part 4 Exercise 8 A spruce

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,14 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def spruce(number):
+    print("a spruce!")
+    space = (number -1)
+    space2 = (number -1)
+    nb_char = 1
+    while space >= 0:
+        print(f"{" " * space}{nb_char * "*"}")
+        space -= 1
+        nb_char += 2
+    print(f"{" " * space2}*")
 if __name__ == "__main__":
     spruce(5)
