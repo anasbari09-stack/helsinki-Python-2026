7b3b6c208fdc9a8b9a3bdf68fc268b7f1d3acc5f
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 09:54:49 +0000

    Solved Part 4 Exercise 5 A square

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,17 @@
 # Copy here code of line function from previous exercise
-
+def line(number, string):
+    if string == "":
+        print("*" * number)
+    else:
+        print(string[0] * number)
+if __name__ == "__main__":
+    line(5, "xxx")
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    i = 0
+    while i < size:
+        line(size, character)
+        i += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
