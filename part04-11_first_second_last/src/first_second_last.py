1264b42d55020447ef32f09ce806ea7b5131e06f
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 15:59:48 +0000

    solved exercise 11 part 4 First, second and last words

--- part04-11_first_second_last/src/first_second_last.py
+++ part04-11_first_second_last/src/first_second_last.py
@@ -1,7 +1,25 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def first_word(sentence):
+    find1 = sentence.find(" ")
+    return sentence[:find1]
+def second_word(sentence):
+    find1 = sentence.find(" ")
+    find2 = sentence[(find1 +1):].find(" ")
+    if find2 == -1:
+        return sentence[find1 + 1:]
+    else:
+        return sentence[find1 + 1 : find2 + find1 + 1]
+def last_word(sentence):
+    index = (-1)
+    while True:
+        if sentence[index] == " " :
+            return sentence[index + 1:]
+        index -= 1
+
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
     print(first_word(sentence))
     print(second_word(sentence))
     print(last_word(sentence))
+    
