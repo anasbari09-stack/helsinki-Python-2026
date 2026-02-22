3bd413a711a0cbf56b5c6b77365ccf5bbe15be6a
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 10:02:34 +0000

    Solved Part 4 Exercise 6 A triangle

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -1,8 +1,17 @@
 # Copy here code of line function from previous exercise
-
+def line(number, string):
+    if string == "":
+        print("*" * number)
+    else:
+        print(string[0] * number)
+if __name__ == "__main__":
+    line(5, "xxx")
 def triangle(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    i = 1
+    while i <= size:
+        line(i, "#")
+        i += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
