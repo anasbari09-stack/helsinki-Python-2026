0bfaf4e97d6c9f34b9ba597c67df4234e703bceb
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 08:35:12 +0000

    solved part 4 exercise 02_line

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -1,4 +1,9 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def line(number, string):
+    if string == "":
+        print("*" * number)
+    else:
+        print(string[0] * number)
 if __name__ == "__main__":
-    line(5, "x")
+    line(5, "xxx")
