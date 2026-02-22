8457c498ab5d0da59509c3e7f09f1f596d73e6cf
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 09:45:37 +0000

    Solved Part 4 Exercise 4 A square of hashes

--- part04-04_square_of_hashes/src/square_of_hashes.py
+++ part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,18 @@
 # Copy here code of line function from previous exercise
-
+def line(number, string):
+    if string == "":
+        print("*" * number)
+    else:
+        print(string[0] * number)
+if __name__ == "__main__":
+    line(5, "xxx")
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    i = 0
+    while i < size:
+        line(size, "#")
+        i += 1
+        
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
