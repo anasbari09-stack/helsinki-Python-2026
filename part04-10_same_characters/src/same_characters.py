6ffc7f5bde3f0419567e2a0776bce62fa0ecaf06
anasbari09-stack <anasbari09@gmail.com>
2026-02-22 14:30:10 +0000

    solved part 4 ex 10 Same characters

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,11 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def same_chars(word, char1, char2):
+    if char1 >= len(word) or char2 >= len(word):
+        return False
+    elif word[char1] == word[char2]:
+        return True
+    else:
+        return False
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    same_chars("coder", 1, 10)
