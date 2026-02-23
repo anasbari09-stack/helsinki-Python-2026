705b9d4e8fe8438eeb2868e9d1bec733cd3cf472
anasbari09-stack <anasbari09@gmail.com>
2026-02-23 12:19:11 +0000

    solved exercise 12 Part 4 Change the value of an item

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,12 @@
 # Write your solution here
+lists = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    if index == -1:
+        break
+    new_value = int(input("New value: "))
+    lists[index] = new_value
+    print(lists)
+
+
