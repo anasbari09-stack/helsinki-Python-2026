476115ce82bc8df757b5ded28355fffb1edef6a4
anasbari09-stack <anasbari09@gmail.com>
2026-02-23 15:25:13 +0000

    Solved Part 4 Exercise 16 List twice

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,11 @@
 # Write your solution here
+numbers =[]
+
+while True:
+    number = int(input("New item: "))
+    if number == 0:
+        print("Bye!")
+        break
+    numbers.append(number)
+    print(f"The list now: {numbers}")
+    print(f"The list in order: {sorted(numbers)}")
