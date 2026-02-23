cbf4bc118a2fda09f189c273ddfe1f13c27217f9
anasbari09-stack <anasbari09@gmail.com>
2026-02-23 12:30:04 +0000

    solved part 4 exercise 13 Add items to a list

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,10 @@
 # Write your solution here
+numbers = []
+items = int(input("How many items: "))
+i = 1
+while i <= items:
+    item = int(input(f"Item {i}: "))
+    numbers.append(item)
+    i += 1
+print(numbers)
+
