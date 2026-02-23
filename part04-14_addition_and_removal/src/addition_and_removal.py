f4c6a283b86ef25c074af4068571e194d685f611
anasbari09-stack <anasbari09@gmail.com>
2026-02-23 14:36:36 +0000

    Solved part 4 exercise 14 Addition and removal

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,16 @@
 # Write your solution here
+numbers = []
+while True:
+    print(f"The list is now {numbers}")
+    choise = input("a(d)d, (r)emove or e(x)it: ")
+    if choise == "d":
+        if numbers == []:
+            numbers.append(1)
+        else:
+            numbers.append(numbers[-1] + 1)
+
+    elif choise == "r" and numbers != []:
+        numbers.pop(-1)
+    elif choise == "x":
+        print("Bye!")
+        break
