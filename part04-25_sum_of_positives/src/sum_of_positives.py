be17709789fe8e3775b0431885d37e1265e192d6
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 10:28:10 +0000

    Solved Part 4 Exercise 25 The sum of positive numbers

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def sum_of_positives(my_list):
+    summe = 0
+    for item in my_list:
+        if item > 0:
+            summe += item
+    return summe
+
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
+        
