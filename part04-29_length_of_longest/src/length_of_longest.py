0d0c266b7afc09155cfcc13ddbafe040f5a53cbe
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 13:55:13 +0000

    Solved Part 4 exercise 29 The length of the longest in the list

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def length_of_longest(my_list):
+    long_string = my_list[0]
+    for i in range(0, len(my_list)):
+        if len(my_list[i]) > len(long_string):
+            long_string = my_list[i]
+    return len(long_string)
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+
+    result = length_of_longest(my_list)
+    print(result)
