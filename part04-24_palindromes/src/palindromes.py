f58e4ae5921e992626c04fcf788aa29f0e158884
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 10:14:39 +0000

    Solved Part 4 Exercise 24 Palindromes

--- part04-24_palindromes/src/palindromes.py
+++ part04-24_palindromes/src/palindromes.py
@@ -2,3 +2,22 @@
 # Note, that at this time the main program should not be written inside
 # if __name__ == "__main__":
 # block!
+def palindromes(word):
+    palindrome = ""
+    for i in range(-1, -len(word) -1, -1):
+        palindrome += word[i]
+    if word == palindrome:
+        return True
+    else :
+        return False
+    
+
+
+while True:
+    word = input("Please type in a palindrome: ")
+    if palindromes(word) is True :
+        print(f"{word} is a palindrome!")
+        break
+    else:
+        print("that wasn't a palindrome")
+    
