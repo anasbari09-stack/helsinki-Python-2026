ab0fcd8a6ccec01efe00c527c5b54866ba9fc4d4
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 12:04:18 +0000

    Solved Part 4 Exercise 28 Distinct numbers

--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def distinct_numbers(my_list):
+    new_list = []
+    for i in range(0, len(my_list)):
+        if my_list[i] not in new_list:
+            new_list.append(my_list[i])
+    return sorted(new_list)
+
+if __name__ == "__main__":
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list)) 
