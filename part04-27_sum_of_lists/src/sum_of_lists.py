4898618cbce13935582761f05d1e177a526de0a3
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 11:51:58 +0000

    solved part 4 exercise 27 The sum of lists

--- part04-27_sum_of_lists/src/sum_of_lists.py
+++ part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def list_sum(list_1, list_2):
+    list_sum = []
+    for i in range(0, len(list_1)):
+        list_sum.append(list_1[i] + list_2[i])
+    return list_sum 
+
+if __name__ == "__main__":
+    list_1 = [1, 2, 3]
+    list_2 = [7, 8, 9]
+    print(list_sum(list_1, list_2))
