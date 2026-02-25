38ddd7109434431fbdda8e934e8db1333fc1b98d
anasbari09-stack <anasbari09@gmail.com>
2026-02-25 14:06:47 +0000

    solved part 4 exercise 30 The shortest in the list

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def shortest(my_list):
+    shortest = my_list[0]
+    for i in range(0, len(my_list)):
+        if len(my_list[i]) < len(shortest):
+            shortest = my_list[i]
+    return shortest
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+
+    result = shortest(my_list)
+    print(result)
