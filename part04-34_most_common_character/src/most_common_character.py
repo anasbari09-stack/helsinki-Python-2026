9ed4de1e45cd0d9f186483ac7d43bfa2edef1185
anasbari09-stack <anasbari09@gmail.com>
2026-02-27 10:59:00 +0000

    Solved Part 4 Exercise 34 Most common character

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,18 @@
-# Write your solution here
+def most_common_character(string):
+    big_count = 0
+    big_char = ""
+    for i in string:
+        if string.count(i) > big_count:
+            big_count = string.count(i)
+            big_char = i
+        elif string.count(i) == big_count:
+            if string.find(i) < string.find(big_char):
+                big_char = i
+    return big_char
+
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+    
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
