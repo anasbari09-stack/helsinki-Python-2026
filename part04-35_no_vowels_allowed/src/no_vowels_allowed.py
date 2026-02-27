92861ddec17ea6543083b873e4cb8761ced545d3
anasbari09-stack <anasbari09@gmail.com>
2026-02-27 11:26:18 +0000

    Solved Exercise Part 4 Exercise 35 No vowels allowed

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def no_vowels(string):
+    new_string = ""
+    voels = "aeiou"
+    for char in string:
+        if char not in voels:
+            new_string += char
+    return new_string
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
