90494584310564fb3f41226dc3a4061d65f2ca28
anasbari09-stack <anasbari09@gmail.com>
2026-02-27 14:42:56 +0000

    Solved Part 4 Exercise 36 No shouting allowed

--- part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def no_shouting(my_list):
+    new_list = []
+    for item in my_list:
+        if item.isupper() == False:
+            new_list.append(item) 
+    return new_list
+
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
