34ba5439eb28493e26d00ed943d57ee66bb4562e
anasbari09-stack <anasbari09@gmail.com>
2026-03-02 08:38:11 +0000

    feat: Solved Part 5 Exercise 6 Sudoku: check block

--- part05-06_sudoku_block/src/sudoku_block.py
+++ part05-06_sudoku_block/src/sudoku_block.py
@@ -1,1 +1,32 @@
 # Write your solution here
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    seen = []                                       # for stock and check numbers if repeat
+    for i in range(row_no, row_no + 3):             # loop with 3 rows start from the row give user 
+        row = sudoku[i]                             
+        for i in range(column_no, column_no + 3):   
+            number = row[i]                         # number start from row and column give user 
+            if number > 0:
+                if number in seen:                  # if number we already stocks in seen return false because is repeat
+                    return False
+                seen.append(number)                 # add number to seen list after condition because if we add before return will give us False every time because we add number before check
+    return True
+
+
+
+
+
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(block_correct(sudoku, 0, 0))
+    print(block_correct(sudoku, 1, 2))
