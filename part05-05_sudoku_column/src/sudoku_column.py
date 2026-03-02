6db69190cfa2c8e49ecb805c77baad72778e5a7d
anasbari09-stack <anasbari09@gmail.com>
2026-03-02 07:03:43 +0000

    feat: Solved Part 5 exercises 4 Sudoku: check row and and 5 Sudoku: check column

--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,27 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int):
+    column_list = []
+    for row in sudoku:
+        column_list.append(row[column_no])
+    for square in column_list:
+        if square > 0:
+            count = column_list.count(square)
+            if count > 1:
+                return False
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
