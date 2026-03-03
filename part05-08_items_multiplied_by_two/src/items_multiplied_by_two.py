68cd7b1e7e08e4b3cf6bd71d059499bd22eed1e3
anasbari09-stack <anasbari09@gmail.com>
2026-03-03 08:32:40 +0000

    feat: Solved Part 5 Exercise 8 Items multiplied by two

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def double_items(numbers: list):
+    new_numbers = numbers[:]                # i did numbers[:] for new_numbers be just a copy and don't have same adress 
+    for i in range(len(new_numbers)):       # if i change item in new_numbers not affect numbers list
+        new_numbers[i] *= 2
+    return new_numbers
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
