85dae142451c2b9fb799d4342fb2ae5d95cd87b6
anasbari09-stack <anasbari09@gmail.com>
2026-03-04 13:56:56 +0000

    feat: Solved Part 5 section Dictionary Exercise 1 Times ten

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def times_ten(start_index: int, end_index: int):
+    numbers_dic = {}
+    for i in range(start_index , end_index + 1):
+        numbers_dic[i] = i * 10
+    return numbers_dic
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
