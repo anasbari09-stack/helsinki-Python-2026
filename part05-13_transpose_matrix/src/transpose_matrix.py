cd9b2af87e6b59ccd3ac2806ec645d69257e9bf4
anasbari09-stack <anasbari09@gmail.com>
2026-03-04 09:09:45 +0000

    feat: Solved Part 5 Exercise 13 Transpose a matrix

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def transpose(matrix: list):
+    copy_list = []
+    for item in matrix:
+        copy_list.append(item[:])
+    for c in range(0, len(matrix)):
+        for r in range(0, len(matrix)):
+            
+            matrix[r][c] = copy_list[c][r]
+    
+
+
+if __name__ == "__main__":
+    my_list = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
+    print(transpose(my_list))
