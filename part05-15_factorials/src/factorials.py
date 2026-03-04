ea5dbe1e3e77cfbec306f5c0fd695a4b7f6d9770
anasbari09-stack <anasbari09@gmail.com>
2026-03-04 14:09:44 +0000

    feat: Solved Part 5 section dictionary Exercise 15 Factorials

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def factorials(n: int):
+    factorials_dic = {}
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+        factorials_dic[i] = factorial
+    return factorials_dic
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
