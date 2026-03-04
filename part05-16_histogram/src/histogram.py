7c5264a991cd708447fab979db80a39158f107b8
anasbari09-stack <anasbari09@gmail.com>
2026-03-04 15:42:13 +0000

    Solved Part 6 Exercise 16 Histogram

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def histogram(string):
+    histogram_dic = {}
+    for s in string:
+        if s not in histogram_dic:
+            histogram_dic[s] = 0
+        histogram_dic[s] += 1
+    for key, value in histogram_dic.items():
+        print(f"{key} {"*" * value}")
+
+if __name__ == "__main__":
+    histogram("statistically")
