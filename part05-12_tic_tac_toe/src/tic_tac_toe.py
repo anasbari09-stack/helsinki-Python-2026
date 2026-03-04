a7dcc5bccfefdd4749faba5114317c2d3ef9d242
anasbari09-stack <anasbari09@gmail.com>
2026-03-04 08:25:39 +0000

    feat: Solved Part 5 Exericise 12 Tic-Tac-Toe

--- part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def play_turn(game_board: list, x: int, y: int, piece: str):
+    if 0 > y or y > 2 or  0 > x or x > 2 :
+        return False
+    
+    row = game_board[y]
+    if row[x] != "":
+        return False 
+    else:
+        row = game_board[y]
+        row[x] = piece
+        return True
+    
+    
+if __name__ == "__main__":
+    game_board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    print(play_turn(game_board, 1, 0, "O"))
+    print(play_turn(game_board, 0, 1, "O"))
+    print(play_turn(game_board, 0, 2, "X"))
+    print(play_turn(game_board, 3, 3, "O"))
+    print(game_board)
