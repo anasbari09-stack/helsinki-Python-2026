94fff5a3ffef8ab043603913ca9eb5f9ee8eaf00
anasbari09-stack <anasbari09@gmail.com>
2026-03-05 09:44:37 +0000

    feat: Solved Part 5 Exercise 19 Invert a dictionary

--- part05-19_invert_dictionary/src/invert_dictionary.py
+++ part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def invert(dictionary: dict):
+    new_dic = {}
+    key_list = []
+    values_list = []
+    for key, value in dictionary.items():
+        key_list.append(value)
+        values_list.append(key)
+    dictionary.clear()
+    for i in range(0, len(values_list)):
+        dictionary[key_list[i]] = values_list[i]
+    
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    invert(s)
+    print(s)
