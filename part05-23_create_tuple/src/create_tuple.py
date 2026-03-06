476669c80c420f8616e161dcf7f392146fbc267b
anasbari09-stack <anasbari09@gmail.com>
2026-03-06 11:49:29 +0000

    feat: Solved part 5 exercise 23

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def create_tuple(x: int, y: int, z: int):
+    my_list = [x, y, z]
+    tuple_create = (min(my_list), max(my_list), sum(my_list))
+    return tuple_create
+
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
