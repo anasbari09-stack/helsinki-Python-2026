2f80d77c1f29b7241c61deebf6175eff0179e88c
anasbari09-stack <anasbari09@gmail.com>
2026-03-06 10:00:13 +0000

    feat: Solved Part 5 Section Dictionary Exercise 22 Find movies

--- part05-22_find_movies/src/find_movies.py
+++ part05-22_find_movies/src/find_movies.py
@@ -1,1 +1,19 @@
 # Write your solution here
+def  find_movies(database: list, search_term: str):
+    new_list = []
+    for item in database:
+        clean_item = item["name"]
+        clean_item = clean_item.lower()
+        if clean_item.find(search_term) != -1:
+            new_list.append(item)
+    return new_list
+            
+
+if __name__ == "__main__":
+    database = [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+    {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94},
+    {"name": "Dawn of the Dead Programmers", "director": "M. Night Python", "year": 2011, "runtime": 101}]
+
+    my_movies = find_movies(database, "python")
+    print(my_movies)
+    
