c71c52207cb57db8da5d162cdca5abd361b05918
anasbari09-stack <anasbari09@gmail.com>
2026-03-06 12:17:24 +0000

    feat: Solve Part 5 Exercise 24 The oldest person

--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def oldest_person(people: list):
+    oldest = people[0][1]
+    name_old = people[0][0]
+    for item in people:
+        if item[1] < oldest:
+            name_old = item[0]
+            oldest = item[1]
+    return name_old
+
+if __name__ == "__main__":
+    people_list = [('Emily', 2014), ('Arthur', 1977)]
+    result = oldest_person(people_list)
