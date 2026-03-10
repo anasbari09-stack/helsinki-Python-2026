a1b58b7cde3126c8a1f62c678a9e30c649ee2d8b
anasbari09-stack <anasbari09@gmail.com>
2026-03-10 08:58:30 +0000

    feat: Solved Part 6 Reading Files Section Exercise 02 Fruit market

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,14 @@
 # write your solution here
+def read_fruits():
+    with open("fruits.csv") as new_file:
+        fruits_dic = {}
+        for fruit in new_file:
+            fruit = fruit.replace("\n", "")
+            parts = fruit.split(";")
+            key = parts[0]
+            value = parts[1]
+            fruits_dic[key] = float(value)
+    return fruits_dic
+
+if __name__ == "__main__":
+    print(read_fruits())
