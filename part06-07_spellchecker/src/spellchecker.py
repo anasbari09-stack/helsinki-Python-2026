7a6afcbc3365061fe39a9b6c708bbb5cbc572481
anasbari09-stack <anasbari09@gmail.com>
2026-03-13 10:43:23 +0000

    feat: Solved Part 6 Reading Files Section Exercise 07 Spell checker

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,13 @@
 # write your solution here
+text = input("Write text: ")
+text_list = text.strip()
+text_list = text_list.split(" ")
+word_dic = {}
+with open("wordlist.txt") as f:
+    for word in f:
+        word = word.strip()
+        word_dic[word] = word
+    for word in text_list:
+        if word.lower() not in word_dic:
+           word = f"*{word}*"
+        print(word + " ",end="") 
