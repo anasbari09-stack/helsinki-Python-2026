fc91192a83cc7ad2f889f27a651dbef726bcc5e2
anasbari09-stack <anasbari09@gmail.com>
2026-03-15 10:11:26 +0000

    feat: Solved Part 6 Section Reading files Exercise 09 Recipe search

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,65 @@
 # Write your solution here
+def search_by_name(filename: str, word: str):
+    found_recipes =  []
+    with open(filename) as f:
+        attemps = 1
+        for line in f:
+            line = line.strip()
+            line_normalise = line.lower()
+            if line == "":
+                attemps = 1
+                continue
+            elif attemps == 1:
+                if line_normalise.find(word) != -1:
+                    found_recipes.append(line)
+            attemps += 1 
+    return found_recipes  
+
+def search_by_time(filename: str, prep_time: int):
+    recipes = []
+    with open(filename) as f:
+        attemps = 1
+        for line in f:
+            line = line.strip()
+            if line == "":
+                attemps = 1
+                continue
+            elif attemps == 1:
+                recipe = line
+            elif attemps == 2 :
+                time = int(line)
+                if time <= prep_time:
+                    recipes.append(f"{recipe}, preparation time {time} min")
+            attemps += 1
+        return recipes
+
+def search_by_ingredient(filename: str, ingredient: str):
+    recipes = []
+    with open(filename) as f:
+        attemps = 1
+        for line in f:
+            line = line.strip()
+            if line == "":
+                attemps = 1
+                continue
+            elif attemps == 1:
+                recipe = line
+            elif attemps == 2 :
+                time = int(line)
+            elif line == ingredient:
+                recipes.append(f"{recipe}, preparation time {time} min")
+            attemps += 1
+        return recipes
+
+
+
+
+
+
+if __name__ == "__main__":
+    found_recipes = search_by_name("recipes2.txt", "oat")
+
+    for recipe in found_recipes:
+        print(recipe)
+
+
