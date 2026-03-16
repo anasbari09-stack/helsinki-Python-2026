e21bc6212010868f56690c706da10e895251ad45
anasbari09-stack <anasbari09@gmail.com>
2026-03-16 09:19:58 +0000

    feat: Solved Part 6 Section Writing Files Exercise 11 Diary

--- part06-11_diary/src/diary.py
+++ part06-11_diary/src/diary.py
@@ -1,1 +1,17 @@
-# Write your solution here
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    function = int(input("Function: "))
+    if function == 0:
+        print("Bye now!")
+        break
+    elif function == 1:
+        diary_entry = input("Diary entry: ")
+        with open("diary.txt", "a") as f:
+            f.write(f"{diary_entry}\n")
+            print("Diary saved")
+    else:
+        print("Entries:")
+        with open("diary.txt") as f:
+            for line in f:
+                line = line.strip()
+                print(line)
