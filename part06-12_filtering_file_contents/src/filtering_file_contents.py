1938aed8b376d16511dfd64b9032ccccee337406
anasbari09-stack <anasbari09@gmail.com>
2026-03-17 14:33:52 +0000

    Solved Part 6 Section Writing Files Exercise 12 Filtering the contents of a file

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,35 @@
 # Write your solution here
+def filter_solutions():
+    with open("correct.csv", "w") as f:
+        pass
+    with open("incorrect.csv", "w") as f:
+        pass
+    with open("solutions.csv") as f:
+        for line in f:
+            parts = line.strip().split(";")
+            operation = parts[1]
+            operation.find("-")
+            operation.find("+")
+
+            if operation.find("+") > operation.find("-"):
+                find = operation.find("+")
+                result = int(operation[0:find]) + int(operation[find+1:])
+            else:
+                find = operation.find("-")
+                result = int(operation[0:find]) - int(operation[find+1:])
+
+            if result == int(parts[2]):
+                
+                with open("correct.csv", "a") as f:
+                    f.write(line)
+            else:
+               
+                with open("incorrect.csv", "a") as f:
+                    f.write(line)
+            
+
+            
+
+
+if __name__ == "__main__":
+    filter_solutions()
