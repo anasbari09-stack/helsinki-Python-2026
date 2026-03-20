9f619d3daee0029acf69f6ef9ef4d81a00e7298a
anasbari09-stack <anasbari09@gmail.com>
2026-03-20 18:18:26 +0000

    feat: Solved Part 6 Reading Files Section Exercise Word search

--- part06-15_word_search/src/word_search.py
+++ part06-15_word_search/src/word_search.py
@@ -1,1 +1,28 @@
-# Write your solution here
+def find_words(search_term: str):
+    list_words = []
+    with open("words.txt") as f:
+        for line in f:
+            line = line.strip()
+            if search_term.startswith("*") == True:
+                if line.endswith(search_term[1:]) == True:
+                    list_words.append(line)
+            elif search_term.endswith("*") == True:
+                if line.startswith(search_term[0:-1]) == True:
+                    list_words.append(line)
+            elif "." in search_term:
+                if len(search_term) == len(line):
+                    valid = True
+                    for i in range(len(search_term)):
+                        if search_term[i] != ".":
+                            if search_term[i] != line[i]:
+                                valid = False
+                    if valid == True:
+                        list_words.append(line)
+            elif line == search_term:
+                list_words.append(line)
+        return list_words
+
+if __name__ == "__main__":
+    print(find_words("cat"))
+
+
