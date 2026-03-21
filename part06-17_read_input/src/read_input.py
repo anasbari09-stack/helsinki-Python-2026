ddff28b1c0c32e2f6c2e88a7f7d02dc13bdfcc44
anasbari09-stack <anasbari09@gmail.com>
2026-03-21 11:24:24 +0000

    feat: Solved Part 6 Section Handling Errors Reading input

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def read_input(sentence: str, nb1:int, nb2: int):
+    while True:
+        try:
+            input_nb = input(sentence)
+            nb = int(input_nb)
+            if nb2 > nb > nb1:
+                return nb
+        except ValueError:
+            pass
+        print(f"You must type in an integer between {nb1} and {nb2}")
+
+if __name__ == "__main__":
+    number = read_input('Enter a number', 1, 5)
+    print("You typed in:", number)
+
+
