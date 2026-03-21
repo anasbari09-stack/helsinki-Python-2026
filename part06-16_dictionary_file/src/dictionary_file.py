f2ebac58e6a2daca3890c6bbc0cd5ad522fdcfc2
anasbari09-stack <anasbari09@gmail.com>
2026-03-21 11:05:24 +0000

    feat: Solved Part 6 Section Writing Files Dictionary stored in a file

--- part06-16_dictionary_file/src/dictionary_file.py
+++ part06-16_dictionary_file/src/dictionary_file.py
@@ -1,1 +1,21 @@
 # Write your solution here
+
+while True:
+    print("1 - Add word, 2 - Search, 3 - Quit")
+    function = int(input("Function: "))
+    if function == 1:
+        word_finnish = input("The word in Finnish: ")
+        word_english = input("The word in English: ")
+        with open("dictionary.txt", "a") as f:
+            f.write(f"{word_finnish} - {word_english}\n")
+        print("Dictionary entry added")
+    elif function == 2:
+        search_term = input("Search term: ") 
+        with open("dictionary.txt") as f:
+            for line in f:
+                parts = line.strip().split("-")
+                if parts[0].find(search_term) != -1 or parts[1].find(search_term) != -1 :
+                    print(line.strip())
+    else:       
+        print("Bye!")
+        break
