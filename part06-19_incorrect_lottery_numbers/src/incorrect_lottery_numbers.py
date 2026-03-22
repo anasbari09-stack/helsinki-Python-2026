eb8d19e40dd95e249a352e0a997d810b113dbb26
anasbari09-stack <anasbari09@gmail.com>
2026-03-22 15:05:20 +0100

    feat: Solved Part 6 Exercise 19 Incorrect lottery numbers

--- part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,24 @@
 # Write your solution here
+def filter_incorrect():
+    with open("lottery_numbers.csv") as f, \
+     open("correct_numbers.csv", "w") as cf:
+        
+        for line in f:
+            parts = line.strip().split(";")
+            part1 = parts[0].split(" ")
+            part2 = parts[1].split(",")
+            try:
+                part1 = int(part1[1])
+                numbers = []
+                error_nb = False
+                for n in part2:
+                    n = int(n)
+                    if n > 39 or n < 1 or n in numbers:
+                        error_nb = True
+                    numbers.append(n)
+                if error_nb  or len(numbers) != 7:
+                    continue
+                with open("correct_numbers.csv", "a") as nf:
+                    nf.write(line)
+            except:
+                continue
