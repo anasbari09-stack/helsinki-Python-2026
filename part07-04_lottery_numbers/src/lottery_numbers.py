99f8d6ab8731272ca137d5a41d70c13a09423d1f
anasbari09-stack <anasbari09@gmail.com>
2026-03-25 18:16:15 +0100

    feat: Solved Part 7 Exercise 04 Lottery numbers

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,8 @@
-# Write your solution here
+from random import randint
+def lottery_numbers(amount: int, lower: int, upper: int):
+    clean_list = []
+    while len(clean_list) < amount:         # the loop stop when he colected the amount of numbers
+        new_random = randint(lower, upper)  # random number between lower and upper
+        if new_random not in clean_list:    # add number to clean_list just if not in the list for don't have the same numbers in the list
+            clean_list.append(new_random)
+    return sorted(clean_list)               # i use sorted function to sorte values from smaller to bigger
