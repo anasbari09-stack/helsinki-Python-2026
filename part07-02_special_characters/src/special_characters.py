7e3369d50aa4f0226da9ee93b7ee11244edd429d
anasbari09-stack <anasbari09@gmail.com>
2026-03-25 16:23:59 +0100

    Solved Part 7 Exercise 02 Special characters

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,16 @@
 # Write your solution here
+import string
+def separate_characters(my_string: str):
+    ascii_letters = ""
+    punctuation = ""
+    others = ""
+    for l in my_string:
+        if l in string.ascii_lowercase or l in string.ascii_uppercase:
+          ascii_letters += l
+        elif l in string.punctuation:
+            punctuation += l
+        else :
+            others += l
+    return (ascii_letters, punctuation, others)
+
+
