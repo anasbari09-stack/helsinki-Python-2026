bc0f5237f8a780c47892cb16d395139e4b9de5af
anasbari09-stack <anasbari09@gmail.com>
2026-03-25 16:42:00 +0100

    feat: Solved Part 7 exercise 03 Fractions

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,8 @@
 # Write your solution here
+from fractions import Fraction
+def fractionate(amount: int):
+    fraction_list = []
+    for i in range(amount):
+        fraction_list.append(Fraction(1, amount))
+    return fraction_list
+
