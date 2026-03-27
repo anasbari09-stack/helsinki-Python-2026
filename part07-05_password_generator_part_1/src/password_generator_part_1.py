94f683f7936f0b2deb1194732a5a0b1a0a07b211
anasbari09-stack <anasbari09@gmail.com>
2026-03-27 22:05:14 +0100

    feat: Solved Part 7 Exercise 05 Password generator, part 1

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,10 @@
 # Write your solution here
+from random import choice
+import string
+
+def generate_password(len_word: int):
+    word = ""
+    for i in range(len_word):
+        word  += choice(string.ascii_lowercase)
+    return word
+
