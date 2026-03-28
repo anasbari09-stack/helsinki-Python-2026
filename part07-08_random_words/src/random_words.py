40fcf3b51fbc451fbde01f71f8a11adea6e710ab
anasbari09-stack <anasbari09@gmail.com>
2026-03-28 10:17:22 +0100

    feat: Solved Part 7 Exercise 08 Random words

--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -1,1 +1,21 @@
 # Write your solution here
+from random import sample
+def words(n: int, beginning: str):
+    list_words = []
+    with open("words.txt") as f:
+        for line in f:
+            word = line.strip()
+            if word.startswith(beginning):
+                list_words.append(word)
+    
+    
+    if len(list_words) < n:
+        raise ValueError("There are not enough words beginning with the specified string")
+    else: 
+        return sample(list_words, n)
+
+if __name__ == "__main__":
+    word_list = words(5, 'car')
+    for word in word_list:
+        print(word)
+        
