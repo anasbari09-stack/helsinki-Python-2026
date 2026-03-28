17830b8b00558171a63a6df719cb0c0f0b1f5743
anasbari09-stack <anasbari09@gmail.com>
2026-03-28 07:57:01 +0100

    feat: Solved Part 7 Exercise 06 Password generator, part 2

--- part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,27 @@
-# Write your solution here
+from random import choice
+import string
+
+def generate_strong_password(len_word: int, numbers:bool , char: bool):
+    lowercase = string.ascii_lowercase
+    digits = string.digits
+    charachters = "!?=+-()#"
+    all_words = lowercase
+    word = choice(lowercase)
+    if numbers:
+       word += choice(digits)
+       all_words += digits
+       
+    if char:
+        word += choice(charachters)
+        all_words += charachters
+    
+    while len(word) < len_word:
+        word += choice(all_words)
+    return word
+
+
+if __name__ == "__main__":
+
+    for i in range(10):
+        print(generate_strong_password(8, True, True))
+
