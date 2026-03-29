fd8cbb1b9b91ff054ec8e36948ec21f09d478ab2
anasbari09-stack <anasbari09@gmail.com>
2026-03-29 11:13:20 +0100

    feat: Solved Part 7 Exercise 10 Valid PIC?

--- part07-10_valid_pic/src/valid_pic.py
+++ part07-10_valid_pic/src/valid_pic.py
@@ -1,1 +1,35 @@
-# Write your solution here
+from datetime import datetime
+def is_it_valid(pic: str):
+    day = int(pic[0:2])
+    month = int(pic[2:4])
+    now = datetime.now()
+    if pic[6] == "+":
+        year = int(f"18{pic[4:6]}")
+    elif pic[6] == "-":
+        year = int(f"19{pic[4:6]}")
+    elif pic[6] == "A":
+        year = int(f"20{pic[4:6]}")
+    else:
+        return False
+    try:
+        birthday = datetime(year, month, day)
+        if birthday > now:
+            return False
+    except:
+        return False
+    comparison_id = int(f"{pic[0:6]}{pic[7:10]}") % 31
+    string = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+    try:
+        if string[comparison_id] != pic[10]:
+            return False
+    except:
+        return False
+    if len(pic) > 11:
+        return False
+    return True
+
+
+
+   
+  
+
