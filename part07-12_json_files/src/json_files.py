7c0eb173a54c2100d419c9d578166cf5fbc74c7b
anasbari09-stack <anasbari09@gmail.com>
2026-03-30 08:46:26 +0100

    feat: Solved part 7 Exercise 12 Handling JSON files

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,14 @@
 # Write your solution here
+import json
+def print_persons(filename: str):
+    with open(filename) as f:
+        data = f.read()
+    
+    students = json.loads(data)
+
+    for student in students:
+        hobbies = ", ".join(student["hobbies"])
+
+        print(f"{student["name"]} {student["age"]} years ({hobbies})")
+
+print_persons("file1.json")
