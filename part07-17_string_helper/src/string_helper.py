21c8a30eb7971803b8c23bfb547e3c6656bab12d
anasbari09-stack <anasbari09@gmail.com>
2026-04-04 15:25:45 +0100

    feat: Solved Part 7 Section 5 Creating your own modules

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,23 @@
 # Write your solution here
+import string
+def change_case(orig_string: str) -> str:
+    new_str = ""
+    for s in orig_string:
+        if s.islower():
+            new_str += s.upper()
+        else:
+            new_str += s.lower()
+    return new_str
+
+def split_in_half(orig_string: str) -> tuple:
+    half_string = len(orig_string) // 2
+    return(orig_string[:half_string], orig_string[half_string:])
+
+def remove_special_characters(orig_string: str) -> str:
+    new_str = "".join(char for char in orig_string if char.isalnum() or char.isspace() )
+    return new_str
+
+if __name__ == "__main__" : 
+    m2 = remove_special_characters("Thi§ is a test test")
+    print(m2)
+
